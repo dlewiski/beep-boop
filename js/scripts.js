92c8986b0cd0eb9556011285607662b339f3fe99
David Lewis <davilewisp@gmail.com>
2018-01-19 14:16:41 -0800

    Testing the proper order of logic to list out all integers if prior conditions are false

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 function numberCheck(number) {
   if (((parseInt(number) % 3) === 0) && (parseInt(number) !== 1) && (parseInt(number) !== 0)) {
     newNumber = "I'm sorry, Dave. I'm afraid you can't do that";
-  };
+  } else if(true) {
   for (i = 0; i < number.length; i++){
-      if (number.charAt(i) === "1") {
-        newNumber = "Boop!";
-        i +=100;
-
-      } if (number.charAt(i) === "0") {
-        newNumber = "Beep!";
-
+    if (number.charAt(i) === "1") {
+      newNumber = "Boop!";
+      i +=100;
+    } if (number.charAt(i) === "0") {
+      newNumber = "Beep!";
       }
     }
+  } else if(true) {
+  listOfNumbers = [];
+  for (digit = 0; digit <= (parseInt(number)); digit++) {
+
+      listOfNumbers[digit] = digit;
+      newNumber = listOfNumbers;
+    }
   }
+  };
+
+
 
 
 
@@ -21,7 +29,6 @@ $(document).ready(function() {
     event.preventDefault();
 
     var number = $("#number").val();
-
     numberCheck(number);
 
     $("#output").text(newNumber)
