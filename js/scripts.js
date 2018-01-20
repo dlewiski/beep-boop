31403e1eb21c5dfc0dd44a6b3a6c714da2edd60d
David Lewis <davilewisp@gmail.com>
2018-01-19 16:57:18 -0800

    Touch up the code to organize and indent as appropraite

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,30 @@
 function numberCheck(number) {
 
-if (((parseInt(number) % 3) === 0)) {
-  for (i = 0; i < number.length; i++){
-    newNumber = "";
-    if (number.charAt(i) === "1") {
-      newNumber = "Boop!"
-      i +=100;
+  if (((parseInt(number) % 3) === 0)) {
+    for (i = 0; i < number.length; i++){
+      newNumber = "";
+      if (number.charAt(i) === "1") {
+        newNumber = "Boop!"
+        i +=100;
 
-    } if (number.charAt(i) === "0") {
-      newNumber = "Beep!";
-
+      } if (number.charAt(i) === "0") {
+        newNumber = "Beep!";
+      }
     }
-
-    }
-    console.log(newNumber);
-}
+  }
   if (((parseInt(number) % 3) === 0) && (parseInt(number) !== 1) && (parseInt(number) !== 0)) {
     newNumber = "I'm sorry, Dave. I'm afraid you can't do that";
 
-} if (newNumber !== "Beep!")
-console.log(newNumber);{
-console.log(newNumber);
+  } if (newNumber !== "Beep!") {
     listOfNumbers = [];
     for (digit = 0; digit <= (parseInt(number)); digit++) {
 
-        listOfNumbers[digit] = digit;
-        newNumber = listOfNumbers;
+      listOfNumbers[digit] = digit;
+      newNumber = listOfNumbers;
+      }
+    }
+}
 
-}
-}
-  }
 
 
 //
